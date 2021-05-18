5c960a6d2357266232c529c501d3f85f46599c9d
sandra <sandrarocio12oc@gmail.com>
2021-05-18 17:45:09 -0500

    git add .

--- js/test.js
+++ js/test.js
@@ -1,28 +1,49 @@
+// $(window).scroll(function () {
+  
+//   if ($(window).scrollTop() > 1000) {
+//     $(".x3").removeClass("section-3");
+  
+//   } else {
+//     $(".x3").addClass("section-3");
+   
+//   }
+//   if ($(window).scrollTop() > 1200) {
+//     $(".x2").removeClass("section-2");
+//     $(".x3").addClass("section-3");
+//   } else {
+//     $(".x2").addClass("section-2");
+   
+//   }
+//   if ($(window).scrollTop() > 1400) {
+//     $(".x1").removeClass("section-1");
+//     $(".x2").addClass("section-2");
+//   } else {
+//     $(".x1").addClass("section-1");
+   
+//   }
+// });
+
 $(window).scroll(function () {
-  if ($(window).scrollTop() > 200) {
-    $(".x4").removeClass("section-4"); 
-  } else {
-    $(".x4").addClass("section-4");
-  }
-  if ($(window).scrollTop() > 400) {
-    $(".x3").removeClass("section-3");
-    $(".x4").addClass("section-4");
-  } else {
-    $(".x3").addClass("section-3");
-   
-  }
-  if ($(window).scrollTop() > 600) {
-    $(".x2").removeClass("section-2");
-    $(".x3").addClass("section-3");
-  } else {
-    $(".x2").addClass("section-2");
-   
-  }
-  if ($(window).scrollTop() > 800) {
+  
+  if ($(window).scrollTop() > 1000) {
     $(".x1").removeClass("section-1");
-    $(".x2").addClass("section-2");
+  
   } else {
     $(".x1").addClass("section-1");
+   
+  }
+  if ($(window).scrollTop() > 1500) {
+    $(".x2").removeClass("section-2");
+    $(".x1").addClass("section-1");
+  } else {
+    $(".x2").addClass("section-2");
+   
+  }
+  if ($(window).scrollTop() > 2000) {
+    $(".x3").removeClass("section-3");
+    $(".x2").addClass("section-2");
+  } else {
+    $(".x3").addClass("section-3");
    
   }
 });
